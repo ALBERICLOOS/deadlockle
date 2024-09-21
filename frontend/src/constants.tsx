42caadfe03ec736984938dab4ce08e3d3517924e
ALBERICLOOS <albericloos2003@gmail.com>
2024-09-21 19:26:39 +0200

    add local storage for hero succes

--- frontend/src/constants.tsx
+++ frontend/src/constants.tsx
@@ -2,8 +2,9 @@ export const LOCAL_STORAGE_KEY_HEROES = 'heroes';
 export const LOCAL_STORAGE_KEY_DATE = 'date';
 export const LOCAL_STORAGE_KEY_ABILITIES = 'abilities';
 export const LOCAL_STORAGE_ABILITY_SUCCESS = 'ability_success';
+export const LOCAL_STORAGE_HERO_SUCCESS = 'hero_success';
 
 
-export const DEBUG = true;
+export const DEBUG = false;
 export const green= '#2ecc71';
 export const red= '#e74c3c';
