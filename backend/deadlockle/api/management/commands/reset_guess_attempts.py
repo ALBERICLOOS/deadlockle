8ac69af103683c995bad02f5371456fbc381f499
ALBERICLOOS <albericloos2003@gmail.com>
2024-09-22 13:43:47 +0200

    change column names

--- backend/deadlockle/api/management/commands/reset_guess_attempts.py
+++ backend/deadlockle/api/management/commands/reset_guess_attempts.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         today = timezone.now().date()
-        Hero.objects.all().update(hero_guess_attempts=0)
-        Hero.objects.all().update(ability_guess_attempts=0)
+        Hero.objects.all().update(hero_guesses=0)
+        Hero.objects.all().update(ability_guesses=0)
         self.stdout.write(self.style.SUCCESS(
             f'Successfully reset guess attempts for all heroes on {today}.'))
