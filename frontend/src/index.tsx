a8732bf719ea5df1010fbddcc6ca8a3fb65ff79d
ALBERICLOOS <albericloos2003@gmail.com>
2024-09-22 12:22:48 +0200

    remove theme

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,25 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {CssBaseline} from "@mui/material";
-
-// Create a custom MUI theme with the default font
-const theme = createTheme({
-    typography: {
-        fontFamily: '"Orbitron", sans-serif', // Set Orbitron as the default font
-    },
-});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <App />
-      </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
 
