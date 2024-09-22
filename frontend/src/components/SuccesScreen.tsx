7b92709e1f6920309703c6c3666113b2771bd32a
ALBERICLOOS <albericloos2003@gmail.com>
2024-09-22 13:42:28 +0200

    make reactive

--- frontend/src/components/SuccesScreen.tsx
+++ frontend/src/components/SuccesScreen.tsx
@@ -1,14 +1,36 @@
 import React from "react";
+import { Box, Typography } from '@mui/material';
 
-export default function SuccesScreen(props: {found: boolean}){
+export default function SuccessScreen(props: { found: boolean }) {
     return (
-        <>
-            <h1 className={`found-hero-heading ${props.found ? 'found' : ''} mobile`}>
-                🎉 CORRECT HERO! 🎉
-            </h1>
-            <h1 className={`found-hero-heading ${props.found ? 'found' : ''} desktop`}>
+        <Box textAlign="center">
+            <Typography
+                variant="h1"
+                color="#4CAF50"
+                margin="10px"
+                className={`found-hero-heading ${props.found ? 'found' : ''}`}
+                fontSize={
+                    {
+                        xs: '1.5rem',
+                        sm: '2rem',
+                }}
+                sx={{ display: { xs: 'block', md: 'none' } }} // Only show on xs to sm
+            >
+                🎉 CORRECT HERO! <span className="flip-emoji">🎉</span>
+            </Typography>
+            <Typography
+                variant="h1"
+                color="#4CAF50"
+                margin="10px"
+                className={`found-hero-heading ${props.found ? 'found' : ''}`}
+                sx={{ display: { xs: 'none', md: 'block' } }}
+                fontSize={{
+                    md: '2.5rem',
+                    lg: '3rem',
+                }}
+            >
                 🎉 YOU HAVE FOUND THE CORRECT HERO! <span className="flip-emoji">🎉</span>
-            </h1>
-        </>
-    )
+            </Typography>
+        </Box>
+    );
 }
