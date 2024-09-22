71b138575b1f39ef3dad872344c7cba415c174ff
ALBERICLOOS <albericloos2003@gmail.com>
2024-09-22 12:05:04 +0200

    add theme

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {CssBaseline} from "@mui/material";
+
+// Create a custom MUI theme with the default font
+const theme = createTheme({
+    typography: {
+        fontFamily: '"Orbitron", sans-serif', // Set Orbitron as the default font
+    },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+      </ThemeProvider>
   </React.StrictMode>
 );
 
