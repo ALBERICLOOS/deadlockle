6154c8b8e49205249b57f3f425b05a04b0f6d463
ALBERICLOOS <albericloos2003@gmail.com>
2024-09-23 09:20:21 +0200

    update gitignore

--- backend/deadlockle/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Hero, DailyHero, Ability, DailyAbility
-
-admin.site.register(Hero)
-admin.site.register(DailyHero)
-admin.site.register(Ability)
-admin.site.register(DailyAbility)
